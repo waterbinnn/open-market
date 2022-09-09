fb6d3c7fc215091e183ec4f17af75b12cc32c6ac
waterbinnn <ewaterbin@naver.com>
2022-09-09 17:58:21 +0900

    💄 color constant 파일 생성

---
+++ src/styles/constants/colors.jsx
@@ -0,0 +1,9 @@
+export const colors = {
+    green: '#21BF48',
+    disabled: '#C4C4C4',
+    hovered: '#EFFFF3',
+    white: '#ffffff',
+    dark: '#767676',
+    black: '#000000',
+    border: '#E0E0E0',
+};
