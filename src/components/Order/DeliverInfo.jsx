7bc8400093a0aa6539e7482102036641a21f5557
waterbinnn <ewaterbin@naver.com>
2022-09-15 18:04:22 +0900

    💄 배송정보 UI 구현

--- src/components/Order/DeliverInfo.jsx
+++ src/components/Order/DeliverInfo.jsx
@@ -1,64 +1,90 @@
-import React from 'react';
+import { colors } from '../../styles/constants/colors';
+import {
+    DeliverInfoSection,
+    H2,
+    Form,
+    Fieldset,
+    Legend,
+    WrapperInput,
+    WrapperAddress,
+    WideInput,
+    PhoneInput,
+    Input,
+    Dash,
+} from '../../styles/components/Order/DeliverInfo.style';
+import { SmButton } from '../../styles/modules/_Button';
 
 function DeliverInfo() {
     return (
         <>
-            <section>
-                <h2>배송정보</h2>
-                <form>
-                    <fieldset>
-                        <legend>주문자 정보</legend>
+            <DeliverInfoSection>
+                <H2>배송정보</H2>
+                <Form>
+                    <Fieldset>
+                        <Legend>주문자 정보</Legend>
 
-                        <div>
+                        <WrapperInput>
                             <label htmlFor="name">이름</label>
-                            <input type="text" />
-                        </div>
+                            <Input type="text" />
+                        </WrapperInput>
 
-                        <div>
+                        <WrapperInput>
                             <label htmlFor="phoneNumber">휴대폰</label>
-                            <input type="text" maxLength="3" />
-                            <input type="text" maxLength="4" />
-                            <input type="text" maxLength="4" />
-                        </div>
+                            <PhoneInput type="text" maxLength="3" />
+                            <Dash>-</Dash>
+                            <PhoneInput type="text" maxLength="4" />
+                            <Dash>-</Dash>
+                            <PhoneInput type="text" maxLength="4" />
+                        </WrapperInput>
 
-                        <div>
+                        <WrapperInput>
                             <label htmlFor="name">이메일</label>
-                            <input type="email" />
-                        </div>
-                    </fieldset>
-                </form>
-                <form>
-                    <fieldset>
-                        <legend>배송지 정보</legend>
+                            <Input type="email" />
+                        </WrapperInput>
+                    </Fieldset>
+                </Form>
+                <Form>
+                    <Fieldset>
+                        <Legend>배송지 정보</Legend>
 
-                        <div>
-                            <label htmlFor="name">이름</label>
-                            <input type="text" />
-                        </div>
+                        <WrapperInput>
+                            <label htmlFor="name">수령인</label>
+                            <Input type="text" />
+                        </WrapperInput>
 
-                        <div>
+                        <WrapperInput>
                             <label htmlFor="phoneNumber">휴대폰</label>
-                            <input type="text" maxLength="3" />
-                            <input type="text" maxLength="4" />
-                            <input type="text" maxLength="4" />
-                        </div>
+                            <PhoneInput type="text" maxLength="3" />
+                            <Dash>-</Dash>
+                            <PhoneInput type="text" maxLength="4" />
+                            <Dash>-</Dash>
+                            <PhoneInput type="text" maxLength="4" />
+                        </WrapperInput>
 
-                        <div>
-                            <label htmlFor="address">배송주소</label>
-                            <div>
-                                <input type="text" maxLength={'6'} />
-                                <button>우편번호 조회</button>
-                            </div>
-                            <input type="text" />
-                            <input type="text" />
-                        </div>
-                        <div>
+                        <WrapperInput>
+                            <WrapperAddress>
+                                <label htmlFor="address">배송주소</label>
+                                <div>
+                                    <div>
+                                        <input type="text" maxLength={'6'} />
+                                        <SmButton
+                                            background={`${colors.green}`}
+                                        >
+                                            우편번호 조회
+                                        </SmButton>
+                                    </div>
+                                    <WideInput type="text" />
+                                    <WideInput type="text" />
+                                </div>
+                            </WrapperAddress>
+                        </WrapperInput>
+                        <WrapperInput>
                             <label htmlFor="message">배송메세지</label>
-                            <input type="text" />
-                        </div>
-                    </fieldset>
-                </form>
-            </section>
+                            <WideInput type="text" />
+                        </WrapperInput>
+                    </Fieldset>
+                </Form>
+            </DeliverInfoSection>
         </>
     );
 }
---
+++ src/styles/components/Order/DeliverInfo.style.jsx
@@ -0,0 +1,82 @@
+import styled from '@emotion/styled';
+import { colors } from '../../constants/colors';
+
+export const DeliverInfoSection = styled.section`
+    width: 90%;
+    margin: 0 auto;
+`;
+export const H2 = styled.h2`
+    width: 90%;
+    margin: 0 auto;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 24px;
+    line-height: 30px;
+    padding: 18px 0;
+    border-bottom: 2px solid ${colors.border};
+`;
+export const Form = styled.form`
+    width: 90%;
+    margin: 40px auto;
+`;
+export const Fieldset = styled.fieldset`
+    display: flex;
+    flex-direction: column;
+`;
+export const Legend = styled.legend`
+    padding: 8px 0;
+    border-bottom: 2px solid ${colors.border};
+    width: 100%;
+    font-weight: 500;
+    font-size: 18px;
+    line-height: 23px;
+`;
+export const WrapperInput = styled.div`
+    padding: 8px 0;
+    border-bottom: 2px solid ${colors.border};
+    width: 100%;
+
+    label {
+        display: inline-block;
+        font-weight: 400;
+        font-size: 16px;
+        line-height: 20px;
+        width: 170px;
+    }
+
+    input {
+        height: 40px;
+        border: 1px solid ${colors.border};
+    }
+`;
+
+export const WrapperAddress = styled.div`
+    display: flex;
+
+    input {
+        margin-left: 63px;
+        margin-bottom: 8px;
+    }
+
+    button {
+        display: inline-block;
+        padding: 11px;
+        margin-left: 12px;
+        width: 140px;
+    }
+`;
+export const WideInput = styled.input`
+    width: 800px;
+`;
+export const Dash = styled.span`
+    padding: 0 10px;
+`;
+export const PhoneInput = styled.input`
+    &:first-of-type {
+        width: 80px;
+    }
+    width: 100px;
+`;
+export const Input = styled.input`
+    width: 334px;
+`;
