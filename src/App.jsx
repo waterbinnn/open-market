8895a115b0ef642335543bcbd95616c3c1b42b64
waterbinnn <ewaterbin@naver.com>
2022-10-03 19:38:12 +0900

    🔨 AuthContextProvider 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AuthContextProvider from './context/AuthContext';
 import HomePage from './pages/HomePage';
 import Login from './pages/LoginPage';
 import Signup from './pages/SignupPage';
@@ -12,17 +13,19 @@ import './App.css';
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />}></Route>
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/signup" element={<Signup />}></Route>
-          <Route path="/seller-signup" element={<SellerSignup />}></Route>
-          <Route path="/:postid" element={<ProductDetailPage />}></Route>
-          <Route path="/:username/cart" element={<UserCartPage />}></Route>
-          <Route path="/:username/order" element={<OrderPage />}></Route>
-        </Routes>
-      </BrowserRouter>
+      <AuthContextProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HomePage />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/signup" element={<Signup />}></Route>
+            <Route path="/seller-signup" element={<SellerSignup />}></Route>
+            <Route path="/:postid" element={<ProductDetailPage />}></Route>
+            <Route path="/:username/cart" element={<UserCartPage />}></Route>
+            <Route path="/:username/order" element={<OrderPage />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </AuthContextProvider>
     </>
   );
 }
