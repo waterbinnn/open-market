6d4cd8fd56174565c8ff3fa168e9bf47beb4bb47
waterbinnn <ewaterbin@naver.com>
2022-10-11 17:52:17 +0900

    🔨 Add React Query

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import { QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+
+import { queryClient } from '../../react-query/queryClient';
 import AuthContextProvider from '../../auth/AuthContext';
 import Routers from './Routes';
 
 function App() {
   return (
     <>
-      <AuthContextProvider>
-        <Routers />
-      </AuthContextProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthContextProvider>
+          <Routers />
+          <ReactQueryDevtools />
+        </AuthContextProvider>
+      </QueryClientProvider>
     </>
   );
 }
