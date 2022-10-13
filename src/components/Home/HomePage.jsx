27aa6a2b5fc837a43f972df20b777155b98b2a8d
waterbinnn <ewaterbin@naver.com>
2022-10-13 18:04:32 +0900

    ⚡️ 리액트쿼리 프리패칭 추가

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -2,8 +2,10 @@ import Footer from '../Common/Header';
 import Header from '..//Common/Header';
 import HomeCarousel from './HomeCarousel';
 import ProductList from '../../components/Home/product/ProductList';
+import { usePrefetchProducts } from './hooks/useProducts';
 
 function HomePage() {
+  usePrefetchProducts();
   return (
     <>
       <Header />
