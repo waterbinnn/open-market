0f2fee0189439c724ba9b6cebb3b0b25cd4aff99
waterbinnn <ewaterbin@naver.com>
2022-11-01 15:43:24 +0900

    🥅 Authorizations to heades , JWT 작성

--- src/axiosInstance/index.js
+++ src/axiosInstance/index.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
-
 import { baseUrl } from './constants';
 
 export function getJWTHeader(user) {
-  return { Authorization: `${user.token}` };
+  return { headers: `JWT ${user.token}` };
 }
 
 const config = { baseURL: baseUrl };
+
 export const axiosInstance = axios.create(config);
