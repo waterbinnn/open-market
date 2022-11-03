84a7ec3dad3d9b7d8d90a5ad23630d49620c377d
waterbinnn <ewaterbin@naver.com>
2022-11-03 19:39:09 +0900

    🔥 AuthContextProvider 제거

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,19 +1,15 @@
 import React from 'react';
 import { QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
-
 import { queryClient } from '../../react-query/queryClient';
-import AuthContextProvider from '../../auth/AuthContext';
 import Routers from './Routes';
 
 function App() {
   return (
     <>
       <QueryClientProvider client={queryClient}>
-        <AuthContextProvider>
-          <Routers />
-          <ReactQueryDevtools />
-        </AuthContextProvider>
+        <Routers />
+        <ReactQueryDevtools />
       </QueryClientProvider>
     </>
   );
