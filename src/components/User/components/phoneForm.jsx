b3d15b45a0441053cb71d26929b75c16439b6a8a
waterbinnn <ewaterbin@naver.com>
2022-11-04 17:28:56 +0900

    🔨 휴대폰번호 유효성검사 완료

--- src/components/User/components/phoneForm.jsx
+++ src/components/User/components/phoneForm.jsx
@@ -3,17 +3,25 @@ import { useForm } from 'react-hook-form';
 
 import { PhoneDropDown } from '../../Common/DropDown';
 
+import { colors } from '../../../styles/constants/colors';
 import { SelectBtn } from '../../../styles/components/User/SignupForm.style';
 import {
   InputGroup,
   Label,
   Input,
+  ErrorMsg,
 } from '../../../styles/components/Common/FormStyles.style';
 
-export default function PhoneForm({ register }) {
+export default function PhoneForm({
+  register,
+  getValues,
+  isPhone,
+  setIsPhone,
+}) {
   const { setValue } = useForm({ mode: 'onChange' });
 
   const [isDropDown, setIsDropDown] = useState(false);
+  const [isErrorMsg, setIsErrorMsg] = useState('');
 
   const handleDropDown = (e) => {
     e.preventDefault();
@@ -26,6 +34,27 @@ export default function PhoneForm({ register }) {
     setIsDropDown(false);
   };
 
+  //숫자 4자리 이상 안넘가게 제한 함수
+  function maxLengthCheck(e) {
+    if (e.target.value.length > e.target.maxLength) {
+      e.target.value = e.target.value.slice(0, e.target.maxLength);
+    }
+  }
+
+  // 휴대폰 길이 10자리 이상인지 체크
+  const checkPhoneLength = () => {
+    const phoneNumber =
+      getValues('firstPhoneNum') +
+      getValues('middlePhoneNum') +
+      getValues('lastPhoneNum');
+    if (phoneNumber.length < 10) {
+      setIsPhone(false);
+      setIsErrorMsg('10자리 이상 입력해주세요');
+    } else {
+      setIsPhone(true);
+    }
+  };
+
   return (
     <>
       <Label htmlFor="phone">휴대폰번호</Label>
@@ -33,8 +62,8 @@ export default function PhoneForm({ register }) {
         <SelectBtn
           type="button"
           value={'010'}
-          onClick={handleDropDown}
           {...register('firstPhoneNum')}
+          onClick={handleDropDown}
         >
           <strong>010</strong>
         </SelectBtn>
@@ -46,19 +75,36 @@ export default function PhoneForm({ register }) {
         )}
         <Input
           id="middlePhoneNum"
-          type="text"
+          type="number"
+          minLength="3"
           maxLength="4"
+          onInput={maxLengthCheck}
           name="middle"
-          {...register('middlePhoneNum')}
+          {...register('middlePhoneNum', {
+            onBlur: () => {
+              checkPhoneLength();
+            },
+          })}
         />
+
         <Input
           id="lastPhoneNum"
-          type="text"
+          onInput={maxLengthCheck}
+          type="number"
+          minLength="3"
           maxLength="4"
           name="last"
-          {...register('lastPhoneNum')}
+          {...register('lastPhoneNum', {
+            onBlur: () => {
+              checkPhoneLength();
+            },
+          })}
         />
       </InputGroup>
+
+      {isPhone === false && (
+        <ErrorMsg color={colors.red}>{isErrorMsg}</ErrorMsg>
+      )}
     </>
   );
 }
